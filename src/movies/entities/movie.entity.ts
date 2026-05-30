7198bf69226f38adf2fb13035afa177182f4270d
NgxGiap <nguyenvangiap215@gmail.com>
2026-05-30 09:22:18 +0700

    feat: add docker, postgresql, typeorm config with entities

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,32 @@
-export class Movie {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from 'typeorm';
+import { Showtime } from '../../showtimes/entities/showtime.entity';
+
+@Entity('movies')
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 200 })
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column()
+  duration: number;
+
+  @Column({ length: 50 })
+  genre: string;
+
+  @Column()
+  releaseDate: string;
+
+  @Column({ nullable: true })
+  posterUrl?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Showtime, (showtime) => showtime.movie)
+  showtimes: Showtime[];
+}
